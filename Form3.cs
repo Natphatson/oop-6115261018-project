f0a6c1193e178ecdd33a32dbfac9f1bf03abf494
Natphatson <68212425+Natphatson@users.noreply.github.com>
2020-10-26 10:30:03 +0700

    Add files via upload

---
+++ Form3.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace oop_6115261018_Project
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void btnadd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                string MyConnection2 = "datasource=localhost;port=3306;username=root;password=;database=foundation";
+
+                string Query = "insert into customer(C_Name	,C_Address,C_Tel) " +
+                    "values('" + this.txtC_Name.Text + "','" + this.txtC_Address.Text + "','" + this.txtC_Tel.Text + "');";
+
+                MySqlConnection MyConn2 = new MySqlConnection(MyConnection2);
+
+                MySqlCommand MyCommand2 = new MySqlCommand(Query, MyConn2);
+
+                MySqlDataReader MyReader2;
+
+                MyConn2.Open();
+
+                MyReader2 = MyCommand2.ExecuteReader();
+
+                MessageBox.Show("Save Data");
+
+                while (MyReader2.Read())
+                {
+
+
+
+                }
+
+                MyConn2.Close();
+
+            }
+
+            catch (Exception ex)
+            {
+
+
+
+                MessageBox.Show(ex.Message);
+
+            }
+
+
+        }
+
+        private void btndelet_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                string MyConnection2 = "datasource=localhost;port=3306;username=root;password=;database=foundation";
+
+
+                string Query = "update customer set C_Name='" + this.txtC_Name.Text + "',C_Address='" + this.txtC_Address.Text + "',C_Tel= '" + this.txtC_Tel.Text + "'" + "' where C_ID='" + this.txtC_ID.Text + "';";
+
+
+                MySqlConnection MyConn2 = new MySqlConnection(MyConnection2);
+
+                MySqlCommand MyCommand2 = new MySqlCommand(Query, MyConn2);
+
+                MySqlDataReader MyReader2;
+
+                MyConn2.Open();
+
+                MyReader2 = MyCommand2.ExecuteReader();
+
+                MessageBox.Show("Data Updated");
+
+                while (MyReader2.Read())
+                {
+
+
+
+                }
+
+                MyConn2.Close();//Connection closed here 
+
+            }
+
+            catch (Exception ex)
+            {
+
+
+
+                MessageBox.Show(ex.Message);
+
+            }
+
+        }
+
+        private void btndelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                string MyConnection2 = "datasource=localhost;port=3306;username=root;password=;database=foundation";
+
+                string Query = "delete from customer where C_Name'" + this.txtC_Name.Text + "',C_Address'" + this.txtC_Address.Text + "',C_Tel'" + this.txtC_Tel.Text + "';";
+
+                MySqlConnection MyConn2 = new MySqlConnection(MyConnection2);
+
+                MySqlCommand MyCommand2 = new MySqlCommand(Query, MyConn2);
+
+                MySqlDataReader MyReader2;
+
+                MyConn2.Open();
+
+                MyReader2 = MyCommand2.ExecuteReader();
+
+                MessageBox.Show("Data Deleted");
+
+                while (MyReader2.Read())
+                {
+
+
+
+                }
+
+                MyConn2.Close();
+
+            }
+
+            catch (Exception ex)
+            {
+
+
+
+                MessageBox.Show(ex.Message);
+
+            }
+
+        }
+
+        private void btndisplay_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                string MyConnection2 = "datasource=localhost;port=3306;username=root;password=;database=foundation";
+                string Query = "select * from customer;";
+                MySqlConnection MyConn2 = new MySqlConnection(MyConnection2);
+                MySqlCommand MyCommand2 = new MySqlCommand(Query, MyConn2);
+                MySqlDataAdapter MyAdapter = new MySqlDataAdapter();
+                MyAdapter.SelectCommand = MyCommand2;
+
+
+                DataTable dTable = new DataTable();
+
+
+                MyAdapter.Fill(dTable);
+                dataGridView1.DataSource = dTable;
+
+
+
+
+
+
+                // MyConn2.Close(); 
+
+
+            }
+
+
+            catch (Exception ex)
+            {
+
+
+
+
+
+
+                MessageBox.Show(ex.Message);
+
+
+            }
+
+        }
+
+        private void btnnext_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
